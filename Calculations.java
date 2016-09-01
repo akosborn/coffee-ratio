5f517bdaeecdbb697ae4e391db3b0bccdbed7adc
akosborn <andrewosborn93@gmail.com>
2016-08-31 19:25:47 -0500

    Added variable to change ratio

--- Calculations.java
+++ Calculations.java
@@ -2,21 +2,29 @@ import java.text.*;
 
 class Calculations
 {	
-	double coffee;
-	double water;
-	String result;
+	private double coffee;
+	private double water;
+	private String result;
+	private int specifiedRatio = 12;
 	
 	public String waterToCoffee(double water)
 	{
-		coffee = water / 12.0;
+		coffee = water / specifiedRatio;
 		result = String.format("%.2f", coffee );
 		return result;
 	}
 	
 	public String coffeeToWater(double coffee)
 	{
-		water = coffee * 12.0;
+		water = coffee * specifiedRatio;
 		result = String.format("%.2f", water);
 		return result;
 	}
+	
+	public void setSpecifiedRatio(int specifiedRatio)
+	{
+		this.specifiedRatio = specifiedRatio;
+	}
+	
+	
 }
