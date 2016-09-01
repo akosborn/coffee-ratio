9a87e7bf5cdcc88e47aefd4c480051025123840f
akosborn <andrewosborn93@gmail.com>
2016-08-31 23:14:11 -0500

    Added auto-calculate and ratio list functionality

--- UI.java
+++ UI.java
@@ -1,30 +1,26 @@
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.event.ActionEvent;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class UI extends Application
 {
-    String address;
     TextField coffeeField, waterField;
+    Calculations calculate;
+    WaterTextFieldHandler waterHandler = new WaterTextFieldHandler();
+    CoffeeTextFieldHandler coffeeHandler = new CoffeeTextFieldHandler();
 
     public void start(Stage primaryStage)
     {
         primaryStage.setTitle("Sir Coffee");
         coffeeField = new TextField();
         coffeeField.setPrefColumnCount(10);
-        coffeeField.textProperty().addListener(new CoffeeTextFieldHandler());
+        coffeeField.textProperty().addListener(coffeeHandler);
 
         ComboBox<String> ratioOptions = new ComboBox<String>();
         ratioOptions.getItems().addAll(
@@ -35,15 +31,14 @@ public class UI extends Application
 
         ratioOptions.getSelectionModel().selectedItemProperty().addListener(new ComboBoxEventHandler());
 
-
         waterField = new TextField();
         waterField.setPrefColumnCount(10);
-        waterField.textProperty().addListener(new WaterTextFieldHandler());
+        waterField.textProperty().addListener(waterHandler);
 
         HBox hbox = new HBox(5);
         hbox.setAlignment(Pos.CENTER);
         hbox.getChildren().addAll(coffeeField, ratioOptions, waterField);
-
+        calculate = new Calculations();
 
         primaryStage.setScene(new Scene(hbox, 500, 300));
         primaryStage.show();
@@ -54,7 +49,26 @@ public class UI extends Application
         @Override
         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
         {
-            System.out.println(coffeeField.getText());
+            // System.out.println("CoffeeTextFieldHandler fired.");
+
+            String entry = coffeeField.getText();
+            if(entry.length() >= 1)
+            {
+                try {
+                    Double entryAsDouble = Double.parseDouble(entry);
+                    waterField.textProperty().removeListener(waterHandler);
+                    waterField.setText(calculate.coffeeToWater(entryAsDouble));
+                    waterField.textProperty().addListener(waterHandler);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }else
+            {
+                // System.out.println("coffeeField lenth: 0");
+                waterField.textProperty().removeListener(waterHandler);
+                waterField.setText("");
+                waterField.textProperty().addListener(waterHandler);
+            }
         }
     }
 
@@ -63,16 +77,22 @@ public class UI extends Application
         @Override
         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
         {
+            // System.out.println("ComboBoxEventHandler fired.");
+
             if(observable.getValue().equals("1:8 (cold brew)"))
             {
-
+                calculate.setSpecifiedRatio(8);
             }else if(observable.getValue().equals("1:12"))
             {
-
+                calculate.setSpecifiedRatio(12);
             }else if(observable.getValue().equals("1:16"))
             {
-
+                calculate.setSpecifiedRatio(16);
             }
+
+            String value = coffeeField.getText();
+            coffeeField.setText("");
+            coffeeField.setText(value);
         }
     }
 
@@ -81,7 +101,28 @@ public class UI extends Application
         @Override
         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue)
         {
-            System.out.println(waterField.getText());
+            // System.out.println("WatertextFieldHandler fired.");
+
+            String entry = waterField.getText();
+            if (entry.length() >= 1)
+            {
+                try {
+                    Double entryAsDouble = Double.parseDouble(entry);
+                    // System.out.println(entryAsDouble);
+                    coffeeField.textProperty().removeListener(coffeeHandler);
+                    coffeeField.setText(calculate.waterToCoffee(entryAsDouble));
+                    coffeeField.textProperty().addListener(coffeeHandler);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }else
+            {
+                // System.out.println("waterField length: 0");
+                coffeeField.textProperty().removeListener(coffeeHandler);
+                coffeeField.setText("");
+                coffeeField.textProperty().addListener(coffeeHandler);
+            }
+
         }
     }
 }
