6d8a28b37dcc4269e98fc2fbe8f629eff4bc4522
akosborn <andrewosborn93@gmail.com>
2016-08-31 19:24:50 -0500

    Updated to support JavaFX gui

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		new CoffeeGui().createAndDisplayGui();
+		UI userInterface = new UI();
+		userInterface.launch(UI.class, args);
 	}
 }
